77d5f9b8fe7d71db766a271465919b67588275ac
9ggy <84878036+9ggy@users.noreply.github.com>
2022-02-08 11:56:35 -0500

    Update nftlistmaker.py

--- nftlistmaker.py
+++ nftlistmaker.py
@@ -1,66 +1,77 @@
 from cmd import Cmd
-import requests
+from requests import get
+from colorama import Fore, Style, init
 
-response = requests.get('https://www.rolimons.com/itemapi/itemdetails')
-items = response.json()['items']
-
+# Resets the color of the message after every message sent.
+init(autoreset = True)
 
 class NFTMaker(Cmd):
-    def do_make(self, arg):
-        """Starts the process of making an NFT list."""
+  def do_make(self, arg):
+    """Starts the process of making an NFT list."""
+    items, nftlist = get('https://www.rolimons.com/itemapi/itemdetails').json()['items'], []
 
-        nftlist = []
+    x = input(Fore.GREEN + "All projecteds? (recommended) | y or n ")
+    if x.lower() == "y":
+      for item in items:
+        if items[item][7] == 1:
+          nftlist.append(item)
 
-        x = input("All projecteds? (recommended) -- y or n ")
-        if x.lower() == "y":
-            for item in items:
-                if items[item][7] == 1:
-                    nftlist.append(item)
+    x = input(Fore.LIGHTYELLOW_EX + "\nAll unvalued (RAP) items? | Y or N: ")
+    if x.lower() == "y":
+      for item in items:
+        if items[item][3] == -1:
+          if str(item) in nftlist:
+            pass
+          else:
+            nftlist.append(item)
 
-        x = input("\nAll unvalued (RAP) items? -- y or n ")
-        if x.lower() == "y":
-            for item in items:
-                if items[item][3] == -1:
-                    if str(item) in nftlist:
-                        pass
-                    else:
-                        nftlist.append(item)
+    x = input(Fore.LIGHTMAGENTA_EX + "\nAll Gucci items? | Y or N: ")
+    if x.lower() == "y":
+      for item in items:
+        if "gucci" in items[item][0].lower():
+          if str(item) in nftlist:
+            pass
+          else:
+            nftlist.append(item)
 
-        x = input("\nAll Gucci items? -- y or n ")
-        if x.lower() == "y":
-            for item in items:
-                if "gucci" in items[item][0].lower():
-                    if str(item) in nftlist:
-                        pass
-                    else:
-                        nftlist.append(item)
+    x = input(Fore.BLUE + "\nAll Ralph Lauren items? -- Y or N: ")
+    if x.lower() == "y":
+      for item in items:
+        if "ralph lauren" in items[item][0].lower():
+          if str(item) in nftlist:
+            pass
+          else:
+            nftlist.append(item)
 
-        x = input("\nAll Ralph Lauren items? -- y or n ")
-        if x.lower() == "y":
-            for item in items:
-                if "ralph lauren" in items[item][0].lower():
-                    if str(item) in nftlist:
-                        pass
-                    else:
-                        nftlist.append(item)
-        file = open("nftlist.txt", "w")
-        file.write(", ".join(nftlist))
+    with open("nftlist.txt", "w") as nftfile:
+      nftfile.write(", ".join(nftlist))
 
-    def do_quit(self, arg):
-        """Quits the program."""
-        print("Quitting.")
-        raise SystemExit
+    print(Style.BRIGHT + Fore.WHITE + "\nFinished creating your NFTList, check the folder that the script was run in.")
+    return
 
-    def do_discord(self, arg):
-        """Prints the discord invite link."""
-        print("https://discord.gg/ERUYUCZfWJ")
 
-    def do_credits(self, arg):
-        """Displays the credits for this program."""
-        print("marshall#4949 - development \nnicK#7461 - giving me details on rolimon's api \nEgg#7087 - concept")
+  def do_quit(self, arg):
+    """Quits the program."""
+    
+    print("Quitting.")
+    raise SystemExit
+
+
+  def do_discord(self, arg):
+    """Prints the discord invite link."""
+    
+    print("https://discord.gg/ERUYUCZfWJ")
+    return
+
+
+  def do_credits(self, arg):
+    """Displays the credits for this program."""
+    
+    print("marshall#4949 - development \nnicK#7461 - giving me details on rolimon's api \nEgg#7087 - concept")
+    return
 
 
 if __name__ == '__main__':
-    prompt = NFTMaker()
-    prompt.prompt = '> '
-    prompt.cmdloop('Enter "help" for a list of commands.')
+  prompt = NFTMaker()
+  prompt.prompt = Fore.LIGHTBLUE_EX + '> '
+  prompt.cmdloop('Enter "help" for a list of commands.')
